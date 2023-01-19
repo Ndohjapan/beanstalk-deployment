bdf8c9c1981703c314d7e8b1f93d548ca380e995
Joel <ndohjoelmbj16@gmail.com>
2023-01-19 16:10:28 +0100

    I have made the changes to the file

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ COPY . .
 RUN npm run build
 
 FROM nginx
+
+# Get the compiled app from the first step which is at index 0 and copy it from the build folder into the nginx folder
 COPY --from=0 /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test updates
+          In this world we are living in and it goes 
         </p>
         <a
           className="App-link"
